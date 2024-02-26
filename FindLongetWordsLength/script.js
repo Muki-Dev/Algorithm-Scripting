d1b18b9a9ca5263de1ea015d67b825f6110950ec
Muki Dev <mukidev881@gmail.com>
2024-02-26 09:18:44 +0100

    #AlgoChallenge4: quiz resolve

--- FindLongetWordsLength/script.js
+++ FindLongetWordsLength/script.js
@@ -1,1 +1,12 @@
 
+function findLongestWordLength(str) {
+    const strSplit = str.split(' ');
+    let maxLength = 0;
+    for(let i = 0 ; i < strSplit.length ; i++){
+      if(strSplit[i].length > maxLength)
+        maxLength = strSplit[i].length;
+    }
+    return maxLength;
+  }
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
